f249d82004cd1d0c6a8c3883901ee0746b856176
santosh <sainty05rathore@gmail.com>
2023-04-14 12:29:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Component/Navbar";
+import ChatBox from "./Component/ChatBox";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
